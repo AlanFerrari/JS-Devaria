2d8f029799800a367616f2e1ed822c63cf39ca2f
AlanFerrari <87348265+AlanFerrari@users.noreply.github.com>
2021-07-22 23:43:16 -0300

    primeiro contato com js

---
+++ tipos-dados.js
@@ -0,0 +1,19 @@
+const inteiro = 10;
+const decimal = 10.5;
+const bool = false;
+const nome = "Alan";
+let copo = null;
+let televisão;
+let pessoa = {
+    nome: "Douglas"
+}
+const symb = Symbol()
+
+console.log("tipo da variavel inteiro", typeof inteiro);
+console.log("tipo da variavel decimal", typeof decimal);
+console.log("tipo da variavel bool", typeof bool);
+console.log("tipo da variavel nome", typeof nome);
+console.log("tipo da variavel copo", typeof copo);
+console.log("tipo da variavel televisão", typeof televisão);
+console.log("tipo da variavel pessoa", typeof pessoa);
+console.log("tipo da variavel symb", typeof symb);
